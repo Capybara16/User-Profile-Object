22046f8581b906aec9ffddf30cd3ad20a06009af
Capybara16 <laurentinokris16@gmail.com>
2024-11-04 18:23:25 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,10 @@
+// Create an object to store user profile information
+let userProfile = {
+    fullName: "Kristine Duga Laurentino",                
+    age: 19,                             
+    favoriteNumber: 16,                   
+    favoriteColors: ["black", "grey", "green"] 
+};
+
+// Log the entire user profile object to the console
+console.log(userProfile);
